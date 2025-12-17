aaa11cc339b88b2fc26c32241dc6d7c7cf54bd53
Properti-X <th.kiene@gmail.com>
2025-12-17 10:29:20 +0100

    Logo size h-10

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             <Image
               src="/logo.png"
               alt="CampKit"
-              width={160} height={40} className="h-8 w-auto"
+              width={180} height={45} className="h-10 w-auto"
             />
           </Link>
           <div className="flex items-center gap-4">
@@ -501,6 +501,7 @@ export default function Home() {
     </div>
   )
 }
+
 
 
 
